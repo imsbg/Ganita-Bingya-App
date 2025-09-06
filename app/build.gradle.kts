e6121fa8cc72e49c6ce52e29960e6657264b7883
SandeepBiswalG <sandipbiswal549@gmail.com>
2025-09-06 20:33:53 +0530

    feat: Implement comprehensive multilingual support and refactor for localization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.sandeep.ganitabigyan"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
+        versionCode = 8
         versionName = "5.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
