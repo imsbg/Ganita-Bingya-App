1cdc95daa66cf43bf10111246f8d214728391c82
SandeepBiswalG <sandipbiswal549@gmail.com>
2025-09-18 11:43:38 +0530

    feat: add zero to the numbers list.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.sandeep.ganitabigyan"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "5.1.0"
+        versionCode = 12
+        versionName = "5.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
